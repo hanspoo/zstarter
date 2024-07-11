c5e8fbcd1bd82e84f52d815bff9ea1114950b192
hans <hans@welinux.cl>
2024-07-10 21:20:03 -0400

    Pudiendo logear

--- apps/front/src/app/components/Login.tsx
+++ apps/front/src/app/components/Login.tsx
@@ -9,15 +9,26 @@ const Login = ({ authenticated, handleLogin }: Props) => {
     <div>
       {authenticated === null && <div>Loading...</div>}
       {authenticated === false && (
-        <div>
-          <button
-            onClick={() => {
-              // Perform the authorization request, including the code challenge
-              handleLogin();
-            }}
-          >
-            Login
-          </button>
+        <div className="hero min-h-[640px]">
+          <div className="hero-content text-center">
+            <div className="max-w-md">
+              <h1 className="text-4xl font-bold">Acceso al sistema</h1>
+              <p className="py-6">
+                Provident cupiditate voluptatem et in. Quaerat fugiat ut
+                assumenda excepturi exercitationem quasi. In deleniti eaque aut
+                repudiandae et a id nisi.
+              </p>
+              <button
+                className="btn btn-primary"
+                onClick={() => {
+                  // Perform the authorization request, including the code challenge
+                  handleLogin();
+                }}
+              >
+                Login
+              </button>
+            </div>
+          </div>
         </div>
       )}
       {authenticated && <Navigate to="/admin/callback" />}
--- apps/front/src/app/meta-app.tsx
+++ apps/front/src/app/meta-app.tsx
@@ -1,15 +1,69 @@
 import { Footer, LandingHome, Navbar } from '@freedom/components';
 import { Route, Routes } from 'react-router-dom';
 import App from './app';
+import React, { useEffect, useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { createZitadelAuth, ZitadelConfig } from '@zitadel/react';
+
+import Login from './components/Login';
+import Callback from './components/Callback';
 
 export function MetaApp() {
+  const config: ZitadelConfig = {
+    authority: 'http://localhost:8080/',
+    client_id: '275414623035917827@zitadel-project',
+    redirect_uri: 'http://localhost:4200/admin/callback',
+    post_logout_redirect_uri: 'http://localhost:4200/admin/callback',
+  };
+
+  const zitadel = createZitadelAuth(config);
+
+  function login() {
+    zitadel.authorize();
+  }
+
+  function signout() {
+    zitadel.signout();
+  }
+
+  const [authenticated, setAuthenticated] = useState<boolean | null>(null);
+
+  useEffect(() => {
+    zitadel.userManager.getUser().then((user) => {
+      if (user) {
+        setAuthenticated(true);
+      } else {
+        setAuthenticated(false);
+      }
+    });
+  }, [zitadel]);
+
   return (
     <div className="flex h-screen flex-col justify-between">
       <div>
         <Navbar />
         <Routes>
           <Route path="/" element={<LandingHome />} />
-          <Route path="/admin" element={<App />} />
+          <Route path="/admin/*">
+            <Route
+              path=""
+              element={
+                <Login authenticated={authenticated} handleLogin={login} />
+              }
+            />
+            <Route
+              path="callback"
+              element={
+                <Callback
+                  authenticated={authenticated}
+                  setAuth={setAuthenticated}
+                  handleLogout={signout}
+                  userManager={zitadel.userManager}
+                />
+              }
+            />
+          </Route>
         </Routes>
       </div>
       <Footer />
