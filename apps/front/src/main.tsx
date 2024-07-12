b84cdb0bece35a10ed76b3ba346558568628009f
hans <hans@welinux.cl>
2024-07-12 12:28:56 -0400

    Refactoriza componentes a lib

--- apps/front/src/main.tsx
+++ apps/front/src/main.tsx
@@ -2,8 +2,7 @@ import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
 
 import { BrowserRouter } from 'react-router-dom';
-import { MetaApp } from './app/meta-app';
-import { OrgProvider } from '@freedom/components';
+import { MetaApp, OrgProvider } from '@freedom/components';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
--- libs/components/src/index.ts
+++ libs/components/src/index.ts
@@ -17,3 +17,10 @@ export { Navbar } from './lib/public/navbar';
 
 export { OrgProvider } from './lib/OrgProvider';
 export { FormArticle } from './lib/form-article/form-article';
+
+export * from './lib/home/app';
+export * from './lib/home/auth-component';
+export * from './lib/home/Authenticated';
+export * from './lib/home/meta-app';
+export * from './lib/home/navbar';
+export * from './lib/home/Unauthenticated';
